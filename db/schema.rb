a957ac8a6861e0b51c67faa9ef175e1b9c3e9c22
Carlos Gonzalez <carlosfwrd@gmail.com>
2021-05-17 18:06:45 -0400

    Change types for columns name and email in User

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_17_203147) do
+ActiveRecord::Schema.define(version: 2021_05_17_220457) do
 
   create_table "users", force: :cascade do |t|
-    t.text "name"
-    t.text "email"
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
