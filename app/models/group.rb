aa0a022c55fd65936ee26ad974b63ecaf8cdfc72
Carlos Gonzalez <carlosfwrd@gmail.com>
2021-05-18 16:54:58 -0400

    Add has_many :through association to Group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,8 @@
 class Group < ApplicationRecord
   belongs_to :user
 
+  has_many :groups_film_viewings, dependent: :destroy
+  has_many :film_viewings, through: :groups_film_viewings, dependent: :destroy
+
   validates :name, presence: true, length: { minimum: 5, maximum: 30 }
 end
