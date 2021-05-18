3c2774269601234164fbb8bb812ff5025a490efa
Carlos Gonzalez <carlosfwrd@gmail.com>
2021-05-18 18:13:56 -0400

    Add migration to remove email column from User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_18_204309) do
+ActiveRecord::Schema.define(version: 2021_05_18_221317) do
 
   create_table "film_viewings", force: :cascade do |t|
     t.string "name"
@@ -40,7 +40,6 @@ ActiveRecord::Schema.define(version: 2021_05_18_204309) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
