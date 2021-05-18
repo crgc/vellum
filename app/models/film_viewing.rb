4e1ea64b3abc21b3573bef2045bc99dd185e5689
Carlos Gonzalez <carlosfwrd@gmail.com>
2021-05-18 16:25:30 -0400

    Add validation to FilmViewing

--- app/models/film_viewing.rb
+++ app/models/film_viewing.rb
@@ -1,3 +1,8 @@
 class FilmViewing < ApplicationRecord
   belongs_to :author, class_name: 'User'
+
+  validates :name, presence: true, length: { minimum: 1, maximum: 40 }
+  validates :rating, presence: true, numericality: { greater_than_or_equal_to: 1.0, less_than_or_equal_to: 10.0 }
+
+  scope :most_recent, -> { order('created_at DESC') }
 end
