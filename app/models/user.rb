01ff31942c013aff8155abedefe59f0e66f0ef32
Carlos Gonzalez <carlosfwrd@gmail.com>
2021-05-18 16:28:48 -0400

    Add validations to email and name in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :film_viewings, dependent: :destroy
+
+  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true
 end
