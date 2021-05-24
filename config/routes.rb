bd537bda915c748cb0ae607d0f2ca2e94de789dc
Carlos Gonzalez <carlosfwrd@gmail.com>
2021-05-23 20:51:59 -0400

    Finish LogIn page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,11 @@ Rails.application.routes.draw do
   resources :film_viewings
   resources :users
 
-  root "users#index"
+  get 'login', to: 'sessions#login'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+
+  get 'logout', to: 'sessions#destroy'
+
+  root "sessions#login"
 end
