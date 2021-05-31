5d1453d1caab9b32ed8fcba3d77699f781bc8e7d
Carlos Gonzalez <carlosfwrd@gmail.com>
2021-05-31 19:56:50 -0400

    Add user profile page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def show_errors(model)
+    pluralize(model.errors.count, 'error') if model.errors.any?
+  end
+
+  def show_notice
+    content_tag(:div, notice, class: 'notice-box') if notice
+  end
+
+  def show_alert
+    content_tag(:div, alert, class: 'alert-box') if alert
+  end
 end
