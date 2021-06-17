7db8e85ae1ab640f4dd297a1d902edbfe884b9d0
Carlos Gonzalez <carlosfwrd@gmail.com>
2021-06-17 01:53:52 -0400

    Add scrollbar to main section

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -37,7 +37,7 @@ class GroupsController < ApplicationController
   def update
     respond_to do |format|
       if @group.update(group_params)
-        format.html { redirect_to @group, notice: 'Group was successfully updated.' }
+        format.html { redirect_to @group }
         format.json { render :show, status: :ok, location: @group }
       else
         format.html { render :edit, status: :unprocessable_entity }
